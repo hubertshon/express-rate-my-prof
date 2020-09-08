4cbe90ad6210bbde0c52e7cc07821be2bca2e235
hubert <huby40@gmail.com>
2020-09-08 15:41:29 -0400

    adds cors

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 3000;
 const db = require("./queries");
 const db2 = require("./reviewqueries");
+var cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(
